674d711ed8ee84c24b4dbe111c380b718f4bc5e2
tetron <skim6502@gmail.com>
2024-06-26 13:08:25 -0400

    Added the function getComputerChoice to randomly pick a rock,paper, or scissor

--- ropasc.js
+++ ropasc.js
@@ -1,1 +1,13 @@
+const getComputerChoice = () => {
+    let random;
+    let cacheItem = {
+        0: "scissors",
+        1: "rock",
+        2: "paper"
+    };
+    random = Math.floor(Math.random() * 3)
+    return cacheItem[random];
 
+}
+
+console.log(getComputerChoice());
